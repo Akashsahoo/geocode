6ac152ba1d5319309731435038363bee9dd73238
Akashsahoo <akashsahoo15595@gmail.com>
2018-02-14 00:23:16 +0530

    first commit

---
+++ README.md.txt
@@ -0,0 +1,4 @@
+Assessment of Keri Studio
+# For running this project 
+# you should install  urllib
+# run this program and enter any location you can easily get lat and lng 
---
+++ geocode.py
@@ -0,0 +1,19 @@
+import urllib,json,urllib.request      #for import urllib.request because some times error
+loc=input('enter a any address for know the geocode location :\n')     # for getting input in loc variable
+encode_loc=urllib.parse.quote(loc)     # encoded string 
+url = "https://maps.googleapis.com/maps/api/geocode/json?address="+encode_loc  
+#print(url)
+response=urllib.request.urlopen(url)     #  to get httpresponse
+if(response.code==200):                   # 200 for successfully getting response
+ data=json.loads(response.read().decode())     # convert json string to dictionary 
+
+ if(data['status']=='OK'):              # if input address is correct  then we would get status ok in geocode api 
+  results=data['results']              # we have only two nodes results and second one results 
+                                            # we have  only one child of results 
+  print(results[0]['formatted_address'])           # for accessing formatted_address
+  print(results[0]['geometry']['location'])        #  for getting lat and lng 
+ else:
+    print("wrongly typed address")
+else:
+    print("error")
+  
